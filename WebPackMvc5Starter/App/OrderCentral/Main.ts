ad2d04a86da20eaf3a8121858b9d61170f853954
James Rounce <james.rounce@outlook.com>
2018-09-03 19:52:45 -0700

    starter

---
+++ WebPackMvc5Starter/App/OrderCentral/Main.ts
@@ -0,0 +1,12 @@
+﻿
+
+function getIdFromText(strid: string): number {
+    var result = 0;
+
+    if (strid) {
+        const stAr = strid.split("_");
+        result = Number(stAr[stAr.length - 1]);
+    }
+
+    return result;
+}
---
+++ WebPackMvc5Starter/App/layout.ts
@@ -0,0 +1,9 @@
+﻿//import "./../Content/Site.css";
+import "./css/Site.less";
+
+import "bootstrap"
+//import "jquery"
+//import "jquery-validation"
+//import "jquery-validation-unobtrusive"
+
+var baseUrl = window.location.origin + $("#SiteDirectory").val();
